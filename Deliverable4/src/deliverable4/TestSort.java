edbfb931a239b100c2a63961caca2f288d38069c
John Phillips <j.phillips@email.com>
2015-11-09 20:57:21 -0500

    Test 1000 instead of 100 random arrays

--- Deliverable4/src/deliverable4/TestSort.java
+++ Deliverable4/src/deliverable4/TestSort.java
@@ -25,7 +25,7 @@ public class TestSort
 	@Test
 	public void testArraySort()
 	{
-		for (int i = 0; i < 100; i++)
+		for (int i = 0; i < 1000; i++)
 		{
 			// Generate random array
 			int[] arr = generateRandomArray();
